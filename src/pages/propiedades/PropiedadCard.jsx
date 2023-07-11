0c774261aff9cf8a8273c872bbbb8052a94ce984
Alvaro Japa Salazar <alvarojapasalazar@MacBook-Air-de-Alvaro.local>
2023-07-10 21:00:48 -0500

    scroll card

--- src/pages/propiedades/PropiedadCard.jsx
+++ src/pages/propiedades/PropiedadCard.jsx
@@ -8,7 +8,7 @@ import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 const PropiedadCard = ({ propiedad }) => {
 
     const navigate = useNavigate()
-    
+
     let img = `https://backend.alven-inmobiliaria.com.mx/storage/${propiedad?.id}/${propiedad?.foto[0]?.fotos}`
 
     const verPropiedad = (id) => {
@@ -17,17 +17,18 @@ const PropiedadCard = ({ propiedad }) => {
 
     return (
         <>
-            <div className="card" style={{ width: '18rem', cursor: 'pointer' }} onClick={() => verPropiedad(propiedad?.id)}>                
-                <img className="card-img-top" src={img} alt="Card image cap" />
-                <div className="card-body">
+            <div className="card" style={{ width: '18rem', cursor: 'pointer' }} onClick={() => verPropiedad(propiedad?.id)}>
+                <img className="card-img-top custom-image" src={img} alt="Card image cap" />
+                <div className="card-body scrollable-card-body">
                     <h5 className="card-title">{propiedad?.publicidad?.encabezado}</h5>
-                    <p className="card-text"><FontAwesomeIcon className='descripcion-icono' icon={faMoneyBill}/> Precio: {propiedad?.publicidad?.precio_venta.toLocaleString() + '$'} </p>
+                    <p className="card-text"><FontAwesomeIcon className='descripcion-icono' icon={faMoneyBill} /> Precio: {propiedad?.publicidad?.precio_venta.toLocaleString() + '$'} </p>
                     <p className="card-text">{propiedad?.publicidad?.descripcion} </p>
-                    <p className="card-text"><FontAwesomeIcon className='descripcion-icono' icon={faLocationDot}/>Dirección: {propiedad?.direccion?.estado} - {propiedad?.direccion?.municipio} - {propiedad?.direccion?.colonia} - {propiedad?.direccion?.calle} </p>
+                    <p className="card-text"><FontAwesomeIcon className='descripcion-icono' icon={faLocationDot} />Dirección: {propiedad?.direccion?.estado} - {propiedad?.direccion?.municipio} - {propiedad?.direccion?.colonia} - {propiedad?.direccion?.calle} </p>
                     <p className="card-text">Tipo de propiedad: {propiedad?.general?.tipo_operacion} </p>
-                    <a href={propiedad?.publicidad?.video_url} className="card-text" target='_blank'> <FontAwesomeIcon className='descripcion-icono' icon={faYoutube}/>Video en Youtube</a>
+                    <a href={propiedad?.publicidad?.video_url} className="card-text" target='_blank'> <FontAwesomeIcon className='descripcion-icono' icon={faYoutube} />Video en Youtube</a>
                 </div>
             </div>
+
         </>
     )
 }
